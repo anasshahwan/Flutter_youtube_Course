df9c2834d4a396c8c9de76c72943415aee100794
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2020-01-20 09:29:33 -0500

    Create App Using Flutter Create

---
+++ flutter_youtube_course/android/app/src/main/kotlin/com/mytistapp/flutter_youtube_course/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mytistapp.flutter_youtube_course
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
