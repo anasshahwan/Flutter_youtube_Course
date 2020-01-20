14e52daf746347aa3ecd978eb911142e77e15ea5
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2020-01-20 09:50:38 -0500

    add Assets And Images

--- flutter_youtube_course/lib/main.dart
+++ flutter_youtube_course/lib/main.dart
@@ -1,36 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    drawer: Drawer(
-      child: Center(child: Text("Thank You !"),),
-    ),
-    bottomNavigationBar: BottomAppBar(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+      home: Scaffold(
+        drawer: Drawer(
+          child: Center(
+            child: Text("Thank You !"),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(Icons.accessibility),
+                Icon(Icons.exit_to_app),
+                Icon(Icons.account_circle),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.favorite_border),
+          onPressed: null,
+        ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("My App Title "),
+          actions: <Widget>[
+            Icon(
+              Icons.verified_user,
+              color: Colors.pink,
+            ),
+          ],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-          Icon(Icons.accessibility),
-          Icon(Icons.exit_to_app),
-          Icon(Icons.account_circle),
-
-        ],),
+            Image(
+              height: 200,
+              width: 200,
+              image: NetworkImage(
+                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+            ),
+            Image(
+              height: 200,
+              width: 200,
+              image: AssetImage('images/logo.png')),
+            Center(
+              child: Text("Test My App"),
+            ),
+            Center(
+              child: Text("Test My App"),
+            )
+          ],
+        ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(onPressed: null,),
-    appBar: AppBar(centerTitle: true,
-
-    title: Text("My App Title "),
-
-    actions: <Widget>[
-      Icon(Icons.verified_user,color: Colors.pink,),
-
-    ],
-
-
-    ),
-
-  ),
-));
+    ));
