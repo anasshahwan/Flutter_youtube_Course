f30fc6365d2d6fc5f9c39f0c0e60cf9b2022b5cb
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2020-01-21 17:56:10 -0500

    finish profile ui

--- flutter_youtube_course/lib/main.dart
+++ flutter_youtube_course/lib/main.dart
@@ -7,64 +7,335 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        drawer: Drawer(
-          child: Center(
-            child: Text("Thank You !"),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(15),
+            child: ListView(
               children: <Widget>[
-                Icon(Icons.accessibility),
-                Icon(Icons.exit_to_app),
-                Icon(Icons.account_circle),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(Icons.arrow_back),
+                    Icon(Icons.menu),
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        CircleAvatar(
+                          maxRadius: 35,
+                          backgroundImage: AssetImage('images/avatar.png'),
+                        ),
+                        Positioned(right: -3, child: Icon(Icons.verified_user))
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          '@USERNAME',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              fontFamily: 'ComingSoon'),
+                        ),
+                        Text(
+                          'Simple Flutter Ui',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.pin_drop),
+                        Text(
+                          'Current address',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    FlatButton(
+                      onPressed: null,
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.add,
+                            color: Colors.green[500],
+                          ),
+                          Text(
+                            'Follow',
+                            style: TextStyle(color: Colors.green[500]),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          '99',
+                          style: TextStyle(
+                              color: Colors.green[500],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                        Text(
+                          'Rides',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          '400',
+                          style: TextStyle(
+                              color: Colors.green[500],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                        Text(
+                          'Followers',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          '358',
+                          style: TextStyle(
+                              color: Colors.green[500],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                        Text(
+                          'Following',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text('Rider Complements',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            fontFamily: 'ComingSoon')),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Image(
+                              image: AssetImage('images/diamond.png'),
+                              height: 50,
+                              width: 50,
+                            ),
+                            Text(
+                              'Excellent Service',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Image(
+                              image: AssetImage('images/position.png'),
+                              height: 50,
+                              width: 50,
+                            ),
+                            Text(
+                              'Expert Navigation',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Image(
+                              image: AssetImage('images/detergent.png'),
+                              height: 50,
+                              width: 50,
+                            ),
+                            Text(
+                              'Neat And tidy',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Reviews',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'ComingSoon',
+                            fontSize: 20),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/beard.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('images/avatar.png'),
+                          maxRadius: 20,
+                        ),
+                        title: Text('Good Services'),
+                        subtitle: Text('Monday, January 20, 2020'),
+                        trailing: Icon(Icons.more_vert),
+                      ),
+                    ),
+
+
+                  ],
+                )
               ],
             ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.favorite_border),
-          onPressed: null,
-        ),
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("My App Title "),
-          actions: <Widget>[
-            Icon(
-              Icons.verified_user,
-              color: Colors.pink,
-            ),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(
-              height: 200,
-              width: 200,
-              image: NetworkImage(
-                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-            ),
-            Image(
-                height: 200, width: 200, image: AssetImage('images/logo.png')),
-            Center(
-              child: Text(
-                "Test My App",
-                style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 20,
-                    fontFamily: 'ComingSoon',
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Center(
-              child: Text("Test My App"),
-            )
-          ],
         ),
       ),
     );
