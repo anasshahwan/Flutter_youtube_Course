4b4cbda2e55109db245dfa2d59ee88287e1d8a9c
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2020-01-24 12:25:23 -0500

    pass children and statefulWidget

--- flutter_youtube_course/lib/main.dart
+++ flutter_youtube_course/lib/main.dart
@@ -252,7 +252,7 @@ class _MyAppState extends State<MyApp> {
                     myComments.remove(comment);
                     print("Deleted");
                     setState(() {});
-                  },child: Text("Extra Child ..."),);
+                  },child1: Text("Extra Child 1"),child2: Text('Extra child 2.. '),);
                 }).toList(),)
               ],
             ),
@@ -264,27 +264,35 @@ class _MyAppState extends State<MyApp> {
 }
 
 
-class commentsCardWidget extends StatelessWidget {
+class commentsCardWidget extends StatefulWidget {
 
   final Comment comment;
   final Function onClick;
-  final Widget child;
+  final Widget child1;
+  final Widget child2;
 
-  commentsCardWidget({this.comment,this.onClick,this.child});
+  commentsCardWidget({this.comment,this.onClick,this.child1,this.child2});
 
+  @override
+  _commentsCardWidgetState createState() => _commentsCardWidgetState();
+}
 
+class _commentsCardWidgetState extends State<commentsCardWidget> {
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: CircleAvatar(
-          backgroundImage: AssetImage(comment.image_url),
+          backgroundImage: AssetImage(widget.comment.image_url),
           maxRadius: 20,
         ),
-        title: Text(comment.title),
-        subtitle: child,
+        title: Column(children: <Widget>[
+          widget.child1,
+          widget.child2
+        ],),
+        subtitle: Text('subtitle'),
         trailing: GestureDetector(
-            onTap: onClick,
+            onTap: widget.onClick,
             child: Icon(Icons.delete)),
       ),
     );
