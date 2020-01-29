2cf5fb2107ccb14623137ee55ed64681b6169dac
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2020-01-29 15:26:24 -0500

    add Alert Dialoig

--- flutter_youtube_course/lib/main.dart
+++ flutter_youtube_course/lib/main.dart
@@ -18,26 +18,41 @@ class _MyAppState extends State<MyApp> {
 
   ];
 
-  void _showDialog(comment) {
-    print(comment);
-    // flutter defined function
-    showDialog(
+  openMyDialog(comment)  {
+    return showDialog(
       context: context,
+      barrierDismissible: false, // user must tap button!
       builder: (BuildContext context) {
-        // return object of type Dialog
         return AlertDialog(
-          title: new Text("Alert Dialog title"),
-          content: new Text("Alert Dialog body"),
+          backgroundColor: Colors.blue,
+          title: Text('Are You Sure that you want to delete this Card ??..', style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontFamily: 'ComingSoon'),),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+
+              ],
+            ),
+          ),
           actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text("Close"),
+            FlatButton(
+              color: Colors.red,
+
+              child: Text('Yes'),
               onPressed: () {
-                Navigator.of(context).pop();
+
+                        myComments.remove(comment);
+        setState(() {
+        });
+                        Navigator.of(context).pop();
+
               },
             ),
-            new FlatButton(
-              child: new Text("Yes Sure "),
+            FlatButton(
+              color: Colors.green,
+              child: Text('No'),
               onPressed: () {
                 Navigator.of(context).pop();
               },
@@ -48,242 +63,247 @@ class _MyAppState extends State<MyApp> {
       },
     );
   }
-  @override
+
+   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-          child: Container(
-            padding: EdgeInsets.all(15),
-            child: ListView(
-              children: <Widget>[
+    return MaterialApp(
+      home: Scaffold(
+          body: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(15),
+              child: ListView(
+                children: <Widget>[
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Icon(Icons.arrow_back),
-                    Icon(Icons.menu),
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Stack(
-                      children: <Widget>[
-                        CircleAvatar(
-                          maxRadius: 35,
-                          backgroundImage: AssetImage('images/avatar.png'),
-                        ),
-                        Positioned(right: -3, child: Icon(Icons.verified_user))
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          '@Anas shahwan',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              fontFamily: 'ComingSoon'),
-                        ),
-                        Text(
-                          'Simple Flutter Ui',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.pin_drop),
-                        Text(
-                          'Current address',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                    FlatButton(
-                      onPressed: (){
-
-               myComments.add(Comment(image_url:'images/diamond.png' ,title:'test' ,Date: 'Try it ..'));
-
-                setState(() {
-                                 });
-                        },
-                      child: Row(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(Icons.arrow_back),
+                      Icon(Icons.menu),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Stack(
                         children: <Widget>[
-                          Icon(
-                            Icons.add,
-                            color: Colors.green[500],
+                          CircleAvatar(
+                            maxRadius: 35,
+                            backgroundImage: AssetImage('images/avatar.png'),
+                          ),
+                          Positioned(right: -3, child: Icon(Icons.verified_user))
+                        ],
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            '@Anas shahwan',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                fontFamily: 'ComingSoon'),
                           ),
                           Text(
-                            'Follow',
-                            style: TextStyle(color: Colors.green[500]),
+                            'Simple Flutter Ui',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.pin_drop),
+                          Text(
+                            'Current address',
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           )
                         ],
                       ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          '99',
-                          style: TextStyle(
-                              color: Colors.green[500],
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30),
-                        ),
-                        Text(
-                          'Rides',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          '386',
-                          style: TextStyle(
-                              color: Colors.green[500],
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30),
-                        ),
-                        Text(
-                          'Followers',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          '358',
-                          style: TextStyle(
-                              color: Colors.green[500],
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30),
-                        ),
-                        Text(
-                          'Following',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Column(
-                  children: <Widget>[
-                    Text('Rider Complements',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            fontFamily: 'ComingSoon')),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Column(
+                      FlatButton(
+                        onPressed: (){
+
+                 myComments.add(Comment(image_url:'images/diamond.png' ,title:'test' ,Date: 'Try it ..'));
+
+                  setState(() {
+                                   });
+                          },
+                        child: Row(
                           children: <Widget>[
-                            Image(
-                              image: AssetImage('images/diamond.png'),
-                              height: 50,
-                              width: 50,
+                            Icon(
+                              Icons.add,
+                              color: Colors.green[500],
                             ),
                             Text(
-                              'Excellent Service',
-                              style: TextStyle(fontWeight: FontWeight.bold),
+                              'Follow',
+                              style: TextStyle(color: Colors.green[500]),
                             )
                           ],
                         ),
-                        Column(
-                          children: <Widget>[
-                            Image(
-                              image: AssetImage('images/position.png'),
-                              height: 50,
-                              width: 50,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            '99',
+                            style: TextStyle(
+                                color: Colors.green[500],
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          ),
+                          Text(
+                            'Rides',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
                             ),
-                            Text(
-                              'Expert Navigation',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Image(
-                              image: AssetImage('images/detergent.png'),
-                              height: 50,
-                              width: 50,
-                            ),
-                            Text(
-                              'Neat And tidy',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Column(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'Reviews',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'ComingSoon',
-                            fontSize: 20),
+                          )
+                        ],
                       ),
-                    ),
-                    SizedBox(height: 20,),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            '386',
+                            style: TextStyle(
+                                color: Colors.green[500],
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          ),
+                          Text(
+                            'Followers',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            '358',
+                            style: TextStyle(
+                                color: Colors.green[500],
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          ),
+                          Text(
+                            'Following',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text('Rider Complements',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              fontFamily: 'ComingSoon')),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Image(
+                                image: AssetImage('images/diamond.png'),
+                                height: 50,
+                                width: 50,
+                              ),
+                              Text(
+                                'Excellent Service',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Image(
+                                image: AssetImage('images/position.png'),
+                                height: 50,
+                                width: 50,
+                              ),
+                              Text(
+                                'Expert Navigation',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Image(
+                                image: AssetImage('images/detergent.png'),
+                                height: 50,
+                                width: 50,
+                              ),
+                              Text(
+                                'Neat And tidy',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Reviews',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'ComingSoon',
+                              fontSize: 20),
+                        ),
+                      ),
+                      SizedBox(height: 20,),
 
-                  ],
-                ),
-                Column(children: myComments.map((comment){
-                  return commentsCardWidget(comment:comment,onClick: (){
+                    ],
+                  ),
+                  Column(children: myComments.map((comment){
+                    return commentsCardWidget(comment:comment,onClick: (){
 
-                    _showDialog(comment);
-                  },child1: Text("Extra Child 1"),child2: Text('Extra child 2.. '),);
-                }).toList(),)
-              ],
+
+
+        openMyDialog(comment);
+                     },child1: Text("Extra Child 1"),child2: Text('Extra child 2.. '),);
+                  }).toList(),)
+                ],
+              ),
             ),
           ),
         ),
-      );
+    );
   }
 }
 
